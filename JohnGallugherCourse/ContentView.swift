6b92b05ac56b204ad5d7dc9bc59ed7c5393f121a
Praveenrathi0511 <praveenrathik11@gmail.com>
2023-12-18 08:04:16 +0530

    Initial Commit

---
+++ JohnGallugherCourse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JohnGallugherCourse
+//
+//  Created by Praveen rathi on 27/09/1945 Saka.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JohnGallugherCourse/JohnGallugherCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  JohnGallugherCourseApp.swift
+//  JohnGallugherCourse
+//
+//  Created by Praveen rathi on 27/09/1945 Saka.
+//
+
+import SwiftUI
+
+@main
+struct JohnGallugherCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
