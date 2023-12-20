95d3320f35a1a4a39bce516ff40d1b58d1547533
Praveenrathi0511 <praveenrathik11@gmail.com>
2023-12-20 18:57:33 +0530

    Today - 20 dec

--- JohnGallugherCourse/Project1.swift
+++ JohnGallugherCourse/Project1.swift
@@ -8,24 +8,35 @@
 import SwiftUI
 
 struct Project1: View {
-    @State private var messageString = ""
+    @State private var messageString = "Namaste"
     var body: some View {
         VStack{
+            Spacer()
+            Image(systemName: "figure.mind.and.body")
+                .resizable()
+                .scaledToFit()
+                .foregroundStyle(.pink)
             Text(messageString)
                 .font(.largeTitle.weight(.heavy))
                 .foregroundStyle(.red)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+                .frame(height: 150)
+                .frame(maxWidth:.infinity)
                 .padding()
+            Spacer()
             HStack{
                 Button("Awesome") {
                     messageString = "You are Awesome!"
                 }
                 .buttonStyle(.borderedProminent)
-                
+                Spacer()
                 Button("Great") {
                     messageString = "You are Great!"
                 }
                 .buttonStyle(.borderedProminent)
             }
+            .padding()
         }
     }
 }
